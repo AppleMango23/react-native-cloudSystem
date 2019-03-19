d9c1646aa85852b850d8342754d1d1c45372d7e9
Noah Yek <ninjayek@gmail.com>
2019-03-19 21:02:33 +0800

    added nice icon

--- src/pages/ShowSearch.js
+++ src/pages/ShowSearch.js
@@ -46,7 +46,7 @@ export default class Succeed extends Component {
     return (
       <WebView
         // source={{uri: ExampleUrlGoing}}
-        source={{uri: "http://192.168.1.93/Bisston_Cloud/webView/search.php"}}
+        source={{uri: "http://192.168.43.55/Bisston_Cloud/webView/search.php"}}
         // injectedJavaScript={`const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `}
         // right=0,
         scalesPageToFit={false}
--- src/pages/Succeed.js
+++ src/pages/Succeed.js
@@ -1,9 +1,9 @@
 import Biometrics from 'react-native-biometrics'
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text,TextInput, View,Button,ImageBackground} from 'react-native';
+import {Platform, StyleSheet, Text,TextInput, View,Button,ImageBackground,TouchableOpacity,Image,ListView,ScrollView} from 'react-native';
 import { Header,Icon } from 'react-native-elements';
 import {DrawerNavigator} from 'react-navigation';
-
+import JellySideMenu from 'react-native-jelly-side-menu'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -12,55 +12,73 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component{
+export default class JellySideMenuPage extends Component {
   static navigationOptions = {
-    // headerTitle: <Text style={{color:'white',fontSize:20}}>   Bisston</Text>,
-    // headerRight:(
-    //   <View style={{flexDirection:'row'}}>
-    //   <Icon
-    //     name='search'
-    //     size={24}
-    //     color='white'
-    //     // onPress={()=>this.travelToSearch}
-    //     onPress={() => this.setState({changingStatus:"zzzzzz"})}
-
-    //     // onPress={() =>this.travelToSearch}
-        
-    //   />
-    //   <Text>    </Text>
-    //   </View>
-    //   ),
-    // headerLeft:null,  
-    // headerStyle: {
-    //   backgroundColor: '#1E90FF'
-    // },
     header:null
     
   };
-  
   constructor(props) {
-    super(props)
+    super(props);
+    const { navigation } = this.props;
+    const data = navigation.getParam('data', 'nothing inside');
+    const data1 = navigation.getParam('data1', 'nothing inside');
+    const data2 = navigation.getParam('data2', 'nothing inside');
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.itemStyle = {padding: 16, backgroundColor: 'transparent',flexDirection:'row'};
+    this.itemTextStyle = {color: '#000000', fontSize: 17};
+    this.renderMenu = this.renderMenu.bind(this);
     this.state = {
       changingStatus: 'zzz',
+      arrMou: [],
+      username: data,
+      password: data1,
+      side: data2,
+      dataSource: ds.cloneWithRows([]),
+      dialogVisible: false,
+      wordForRename: "",
+      cameraRollUri: null,
+      DeleteDialogVisible: false,
+      wordForDelete: "",
+      searchWord: '',
+      firstLayerfile: true,
+      cancelFor3Selection: false,
+      SecondLayerfile: false,
+      isLoading: true, text: '', //state for searching
+      wordForPopOutNotification: '',//state for popOut Notification
+      layer2Path: "",
+      dialogCreateFolderVisible: false,
+      wordForFolder: "",
+      text: '',
+      checked:false,
+      Noahyek:styles.button1,
     }
   }
-
-  travelToSearch=()=>{
-    this.props.navigation.navigate('ShowSearch')
-    // this.props.navigation.openDrawer();
+ 
+  renderItem(text, iconName,onPress) {
+    return (
+      <TouchableOpacity onPress={onPress}>
+        <View style={this.itemStyle}>
+          <Icon name={iconName} size={24} color='black'/>
+          <Text style={this.itemTextStyle}>{text}</Text>
+        </View>
+      </TouchableOpacity>
+    )
   }
-
-  pressMe=()=>{
-    Biometrics.simplePrompt('Confirm fingerprint')
-  .then(() => {
-    console.log('successful fingerprint provided')
-    this.props.navigation.navigate('Succeed', { data: this.state.username, data1:this.state.password,data2:this.state.side})
-  })
-  .catch(() => {
-    console.log('fingerprint failed or prompt was cancelled')
-  })
+ 
+  renderMenu() {
+    return (
+      <View>
+        <Image style={styles.imageSize} source={require('../images/Logo.png')} />
+        {this.renderItem("  My Drive","cloud-upload", () => {})}
+        {this.renderItem("  Recent file","backspace", () => {})}
+        {this.renderItem("  Upgrade Storage","data-usage", () => {})}
+        {this.renderItem("  Notification","notifications", () => {})}
+        {this.renderItem("  Setting","settings", () => {})}
+        {this.renderItem("  Logout","remove", () => {})}
+        {this.renderItem("  Close Drawer","close", () => {this.jsm.toggleSideMenu(false);})}
+      </View>
+    )
   }
-
   changingStatus=()=>{
     if(this.state.changingStatus === "zzz"){
       return(
@@ -81,52 +99,63 @@ export default class App extends Component{
     }
     
   }
+  travelToSearch=()=>{
+    this.props.navigation.navigate('ShowSearch')
+    // this.props.navigation.openDrawer();
+  }
+  
 
-
+ 
   render() {
     return (
+      <ImageBackground source={require('../images/Background.png')} style={styles.container} >
+        <JellySideMenu 
+        ref={(view) => {this.jsm = view}}
+        fill={"#FFF"} fillOpacity={1.0}
+        renderMenu={this.renderMenu}>
 
-      <ImageBackground source={require('../images/zaza.jpg')} style={styles.container} >
         <Header
-        placement="left"
-        leftComponent={
-          <Icon
-            name='menu'
-            size={24}
-            color='black'
-            onPress={() => this.travelToSearch()}
-          />
-        }
-        centerComponent={{ text: '  Bisston Cloud', style: { color: 'black',fontSize:20 } }}
-        rightComponent={
-          <Icon
-              name='search'
+          placement="left"
+          leftComponent={
+            <Icon
+              name='menu'
               size={24}
               color='black'
-              onPress={() => this.travelToSearch()}
+              // onPress={() => this.travelToSearch()}
             />
-        }
-        style={{flex:1}}
-        containerStyle={{height:'7.5%',paddingBottom:23,backgroundColor:'white'}}
-      />
-      {/* <View style={styles.container}> */}
+          }
+          centerComponent={{ text: '  Bisston Cloud', style: { color: 'black',fontSize:20 } }}
+          rightComponent={
+            <Icon
+                name='search'
+                size={24}
+                color='black'
+                onPress={() => this.travelToSearch()}
+              />
+          }
+          style={{flex:1}}
+          containerStyle={{height:'7.5%',paddingBottom:23,backgroundColor:'white'}}
+        />
+        <ScrollView>
         {this.changingStatus()}
         <Text style={styles.welcome}>Next Screen</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-        <Button
-          onPress={() => this.setState({changingStatus:"pihu"})}
-          title="Press me"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      {/* </View> */}
+        {/* {this.defaultView()} */}
+
+        </ScrollView> 
+ 
+        </JellySideMenu>
       </ImageBackground>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
+  imageSize: {
+    aspectRatio: 0.6,
+    resizeMode: 'contain',
+  },
   container: {
     flex: 1,
     // justifyContent: 'center',
